edd964e63e4869201d4a367a1a4fc7d2ff31635f
Joseph Serunjogi <joeseggie@gmail.com>
2017-03-23 16:43:20 +0300

    Incoming file model configuration.
    Database configuration for the incoming
    file model.
    
    Incoming mail model will hold data about the
    incoming files.
    
    Recorded-by: Joseph Serunjogi <joeseggie@gmail.com>

---
+++ Models/Configuration/IncomingFileConfiguration.cs
@@ -0,0 +1,28 @@
+using MailManager.Extensions;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace MailManager.Models.Configuration
+{
+    public class IncomingFileConfiguration : DbEntityConfiguration<IncomingFile>
+    {
+        public override void Configure(EntityTypeBuilder<IncomingFile> entity)
+        {
+            entity.HasKey(m => m.IncomingFileId);
+            entity.Property(m => m.IncomingFileId)
+                  .ValueGeneratedOnAdd();
+
+            entity.HasOne(m => m.RecordFile)
+                  .WithMany(o => o.IncomingFiles)
+                  .HasForeignKey(m => m.FileNumber);
+            
+            entity.Property(m => m.IncomingDate)
+                  .IsRequired()
+                  .ForSqlServerHasColumnType("date");
+
+            entity.Property(m => m.RowVersion)
+                  .IsRequired()
+                  .IsRowVersion();
+        }
+    }
+}
