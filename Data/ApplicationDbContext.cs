6d1c72dd50a90e41786addfdbad7470690d54716
Joseph Serunjogi <joeseggie@gmail.com>
2017-03-23 17:25:56 +0300

    Database context setup.
    Added the different database models.
    
    Recorded-by: Joseph Serunjogi <joeseggie@gmail.com>

--- Data/ApplicationDbContext.cs
+++ Data/ApplicationDbContext.cs
@@ -5,6 +5,8 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 using MailManager.Models;
+using MailManager.Extensions;
+using MailManager.Models.Configuration;
 
 namespace MailManager.Data
 {
@@ -15,12 +17,26 @@ namespace MailManager.Data
         {
         }
 
+        public DbSet<OfficeMail> OfficeMails { get; set; }
+        public DbSet<IncomingMail> IncomingMails { get; set; }
+        public DbSet<OutgoingMail> OutgoingMails { get; set; }
+        public DbSet<RecordFile> RecordFiles { get; set; }
+        public DbSet<IncomingFile> IncomingFiles { get; set; }
+        public DbSet<OutgoingFile> OutgoingFiles { get; set; }
+        
         protected override void OnModelCreating(ModelBuilder builder)
         {
             base.OnModelCreating(builder);
             // Customize the ASP.NET Identity model and override the defaults if needed.
             // For example, you can rename the ASP.NET Identity table names and more.
             // Add your customizations after calling base.OnModelCreating(builder);
+
+            builder.AddConfiguration(new OfficeMailConfiguration());
+            builder.AddConfiguration(new IncomingMailConfiguration());
+            builder.AddConfiguration(new OfficeMailConfiguration());
+            builder.AddConfiguration(new RecordFileConfiguration());
+            builder.AddConfiguration(new IncomingFileConfiguration());
+            builder.AddConfiguration(new OfficeMailConfiguration());
         }
     }
 }
