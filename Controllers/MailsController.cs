0d1f794577bc8699bd33c6dcd0d40f53f9b46256
Joseph Serunjogi <joeseggie@gmail.com>
2017-03-26 14:06:49 +0300

    Added HTTP post New action method.
    New action method to POST new incoming
    mail.
    
    Recorded-by: Joseph Serunjogi <joseph.serunjogi@hotmail.com>

--- Controllers/MailsController.cs
+++ Controllers/MailsController.cs
@@ -3,6 +3,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using MailManager.Services;
+using MailManager.Models.MailViewModels;
 
 namespace MailManager.Controllers
 {
@@ -22,13 +23,44 @@ namespace MailManager.Controllers
         public IActionResult Index(string search, string mailsearch, string filesearch)
         {
             var mails = _mailService.OfficeMails;
-            mails = mails.Where(m => m.ReferenceNumber.Contains(search)).ToList();
+            if(search != null)
+            {
+                mails = mails.Where(m => m.ReferenceNumber.Contains(search)).ToList();
+            }
             return View(mails);
         }
 
         public IActionResult New()
         {
             return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult New(IncomingMailViewModel newMail)
+        {
+            ModelState.Remove("IncomingMailId");
+            ModelState.Remove("ReferenceNumber");
+            ModelState.Remove("RowVersion");
+            ModelState.Remove("OfficeMail.ReferenceNumber");
+            ModelState.Remove("OfficeMail.RowVersion");
+
+            if(ModelState.IsValid)
+            {
+                newMail.ReferenceNumber = string.IsNullOrWhiteSpace(newMail.ReferenceNumber) ? _mailService.GenerateReferenceNumber() : newMail.ReferenceNumber;
+                newMail.OfficeMail.ReferenceNumber = newMail.ReferenceNumber;
+
+                var newOfficeMailOperation = _mailService.AddOfficeMail(newMail.OfficeMail);
+                var operationResult = newOfficeMailOperation.Success ? _incomingMailService.NewIncomingMail(newMail) : newOfficeMailOperation;
+
+                if(operationResult.Success)
+                {
+                    // TempData["Message"] = operationResult.Message;
+                    return RedirectToAction("index");
+                }
+                ModelState.AddModelError("", operationResult.Message);
+            }
+            return View(newMail);
         }
     }
 }
