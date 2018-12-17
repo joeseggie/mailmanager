0bb52347c329c4557b803b23d5be5f18048bfeed
Joseph Serunjogi <joeseggie@gmail.com>
2018-12-17 15:05:40 +0300

    View model for the roles index view.

---
+++ MailManager.Web/Models/RolesListViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MailManager.Web.Models
+{
+    public class RolesListViewModel
+    {
+        public string Id { get; set; }
+
+        public string Name { get; set; }
+    }
+}
