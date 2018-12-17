0ebd6cb0a215facbbf11f696c06b6e99f463a992
Joseph Serunjogi <joeseggie@gmail.com>
2018-12-17 08:14:05 +0300

    Firstname and Lastname for the application user model.

--- MailManager.Web/Models/ApplicationUser.cs
+++ MailManager.Web/Models/ApplicationUser.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNetCore.Identity;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -8,6 +9,10 @@ namespace MailManager.Web.Models
 {
     public class ApplicationUser : IdentityUser
     {
-        public string Fullname { get; set; }
+        [MaxLength(50)]
+        public string Firstname { get; set; }
+
+        [MaxLength(50)]
+        public string Lastname { get; set; }
     }
 }
