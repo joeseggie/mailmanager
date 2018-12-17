c76f9cb814bce787e365e654b21ef5b56fb0a5a1
Joseph Serunjogi <joeseggie@gmail.com>
2018-12-17 15:29:24 +0300

    View model for the role details view.

---
+++ MailManager.Web/Models/RoleDetailsViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace MailManager.Web.Models
+{
+    public class RoleDetailsViewModel
+    {
+        public string Id { get; set; }
+        public string Name { get; set; }
+        public string NormalizedName { get; set; }
+    }
+}
