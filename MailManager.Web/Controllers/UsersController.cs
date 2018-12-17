f9614906abd7c68bfa00a49166008a312e7e81da
Joseph Serunjogi <joeseggie@gmail.com>
2018-12-17 12:19:26 +0300

    Users details view for the admin. closed #17

---
+++ MailManager.Web/Controllers/UsersController.cs
@@ -0,0 +1,65 @@
+﻿using MailManager.Web.Models;
+using MailManager.Web.Services;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System.Threading.Tasks;
+
+namespace MailManager.Web.Controllers
+{
+    public class UsersController : Controller
+    {
+        private readonly ILogger<UsersController> _logger;
+        private readonly IApplicationUsersService _applicationUsersService;
+
+        public UsersController(ILogger<UsersController> logger, IApplicationUsersService applicationUsersService)
+        {
+            _logger = logger;
+            _applicationUsersService = applicationUsersService;
+        }
+
+        public async Task<IActionResult> Details(string username)
+        {
+            var userDetails = await _applicationUsersService.GetApplicationUserAsync(username);
+            if (userDetails == null)
+            {
+                return NotFound();
+            }
+            var model = new ApplicationUserDetailsViewModel
+            {
+                Firstname = userDetails.Firstname,
+                Email = userDetails.Email,
+                Lastname = userDetails.Lastname,
+                Username = userDetails.Lastname
+            };
+
+            return View(model);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Details(ApplicationUserDetailsViewModel formData)
+        {
+            if (ModelState.IsValid)
+            {
+                var userToUpdate = await _applicationUsersService.GetApplicationUserAsync(formData.Username);
+                if (userToUpdate == null)
+                {
+                    ModelState.AddModelError("Username", "User was not found or does not exist.");
+                }
+                else
+                {
+                    userToUpdate.Firstname = formData.Firstname;
+                    userToUpdate.Lastname = formData.Lastname;
+
+                    var updatedUser = await _applicationUsersService.UpdateUserAsync(userToUpdate);
+
+                    TempData["Message"] = "Changes saved successfully";
+
+                    return RedirectToAction("details", "users", new { username = updatedUser.UserName });
+                }
+            }
+
+            return View(formData);
+        }
+    }
+}
--- MailManager.Web/Services/ApplicationUsersService.cs
+++ MailManager.Web/Services/ApplicationUsersService.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using MailManager.Web.Data;
 using MailManager.Web.Models;
+using Microsoft.EntityFrameworkCore;
 
 namespace MailManager.Web.Services
 {
@@ -17,12 +18,52 @@ namespace MailManager.Web.Services
         }
 
         /// <summary>
+        /// Get application user given their username.
+        /// </summary>
+        /// <param name="username">Username</param>
+        /// <returns>Application user profile.</returns>
+        public async Task<ApplicationUser> GetApplicationUserAsync(string username)
+        {
+            return await _db
+                .Users
+                .SingleOrDefaultAsync(u => 
+                    u.UserName.ToLowerInvariant() == username.ToLowerInvariant()
+                );
+        }
+
+        /// <summary>
         /// Get all the users in the system.
         /// </summary>
         /// <returns>List of application users.</returns>
         public IQueryable<ApplicationUser> GetApplicationUsers()
         {
             return _db.Users;
+        }
+
+        /// <summary>
+        /// Update application user details.
+        /// </summary>
+        /// <param name="applicationUser">Application user update details.</param>
+        /// <returns>Application user.</returns>
+        public async Task<ApplicationUser> UpdateUserAsync(ApplicationUser applicationUser)
+        {
+            var userToUpdate = await _db
+                .Users
+                .SingleOrDefaultAsync(u =>
+                    u.UserName.ToLowerInvariant() == applicationUser.UserName.ToLowerInvariant());
+
+            if (userToUpdate == null)
+            {
+                throw new NullReferenceException("User was not found.");
+            }
+
+            userToUpdate.Firstname = applicationUser.Firstname;
+            userToUpdate.Lastname = applicationUser.Lastname;
+
+            _db.Update(userToUpdate);
+            await _db.SaveChangesAsync();
+
+            return userToUpdate;
         }
     }
 }
